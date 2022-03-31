d25d8c2eb4809a290c427e2aa33c355f1a25839e
Henrike Fleischhack <henrike.fleischhack@googlemail.com>
2022-03-31 13:23:15 -0400

    Test powwer up

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 import pyKeithleyCtl as RC
+from time import sleep
 
 PS = RC.KeithleySupply("169.254.55.31")
 
 print(PS.IDN)
+
+print(PS.get_voltage() )
+print(PS.get_ocp() )
+
+PS.set_voltage(1, 4.2)
+print(PS.get_voltage() )
+
+PS.enable_output(1)
+print(PS.get_voltage() )
+
+sleep(10 )
+
+PS.disble_output(1)
+
+print(PS.get_voltage() )
